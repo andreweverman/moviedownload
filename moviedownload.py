11b6a6bbba028fc31a300f725c7dd0023438d82d
andreweverman <andreweverman@gmail.com>
2021-01-18 17:43:26 -0500

    initial

---
+++ moviedownload.py
@@ -0,0 +1,164 @@
+from mega import Mega
+from clutch import Client
+from requests import Response
+import json
+from clutch.schema.user.method.torrent.add import TorrentAddArguments
+from clutch.schema.user.response.torrent.add import TorrentAdd
+import threading
+import time
+import logging
+import subprocess
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+username = os.getenv("MEGA_EMAIL")
+password = os.getenv("MEGA_PASSWORD")
+ip_addr = os.getenv("TRANSMISSION_IP")
+class MovieClient:
+    def __init__(self):
+        self.mega_client = Mega()
+        self.mega = self.mega_client.login(
+            username, password)
+        self.torrent_client = Client(
+            address=ip_addr)
+
+    def get_mega_files(self) -> list:
+        files = self.mega.get_files()
+        folder_file = None
+        for file_key in files:
+            file = files[file_key]
+            if(file['a']['n'] == 'vvn1'):
+                folder_file = file
+                break
+
+        if(not folder_file):
+            return []
+        file_id = folder_file['h']
+        current_movies = []
+        for file_key in files:
+            file = files[file_key]
+            if file['p'] == file_id:
+                current_movies.append(file)
+
+        return current_movies
+
+    def select_movie(self):
+        movies = self.get_mega_files()
+        movie_str = '\n'.join(["%d: %s" % (i+1, x['a']['n'])
+                               for i, x in enumerate(movies)])
+
+        done = False
+        movie_i = -1
+        while not done:
+            try:
+                user_input = input("Select a movie\n%s\n" % movie_str)
+                movie_i = int(user_input)
+                if(movie_i > 0 and movie_i < len(movies)):
+                    return movies[movie_i]
+            except:
+                print("Try again")
+
+    def add_movie(self):
+        thread = threading.Thread(target=self.download_and_upload)
+        thread.start()
+
+    def check_pia(self):
+        sp_res = subprocess.call('piactl get connectionstate')
+        if (sp_res =='Connected'):
+            return True
+        else:
+            i = 0
+            while i<5:
+                subprocess.call('piactl connect') 
+                time.sleep(1)
+                sp_res = subprocess.call('piactl get connectionstate')
+                if(sp_res=='Connected'):
+                    return True
+        return False
+
+    def download_and_upload(self):
+        pia_conn =self.check_pia()
+        if(not pia_conn):
+            return 'Not connected to VPN. Quitting...'
+        file = self.download_torrent()
+
+        pass
+
+    def download_torrent(self) -> str:
+
+        response = self.add_torrent()
+        if(not response.result == 'success'):
+            raise Exception('Torrent add failed')
+
+        t_hash: str = ''
+        if(response.arguments.torrent_added != None):
+            t_hash = response.arguments.torrent_added.hash_string
+        elif(response.arguments.torrent_duplicate != None):
+            t_hash = response.arguments.torrent_duplicate.hash_string
+
+        downloaded = False
+        print('\n')
+        while(not downloaded):
+            torrent=None
+            torrents = self.get_current_torrents()
+            for tor in torrents:
+                if tor.hash_string == t_hash:
+                    torrent = tor
+
+            if not torrent:
+                raise Exception('No torrent found')
+            print('\r%d percent, %d mbs left' % (torrent.percent_done*100, torrent.left_until_done/1000000),end="")
+            time.sleep(1)
+
+            if(torrent.percent_done==1):
+                downloaded=True
+                print('Download Complete')
+
+        pass
+
+    def add_torrent(self):
+
+        print("Enter the url for the torrent: ")
+        filename: str = input()
+        download_args: TorrentAddArguments = {
+            "filename": filename,
+            "download_dir": '/data/Movies'
+        }
+
+        response: Response[TorrentAdd] = self.torrent_client.torrent.add(
+            download_args)
+        return response
+
+    def get_current_torrents(self) -> list:
+        response = self.torrent_client.torrent.accessor(all_fields=True)
+
+        torrents = response.arguments.torrents
+        return torrents
+
+    def remove_torrents(self):
+        torrents = self.get_current_torrents()
+        torrents_len = len(torrents)
+        if(torrents_len< 1):
+            print('No torrents to remove')
+        else:
+            [self.torrent_client.torrent.remove(
+                torrent.id, False) for torrent in torrents]
+            print("%d Torrents removed" % torrents_len)
+
+
+client = MovieClient()
+# client.download_torrent()
+# client.remove_torrents()
+# client.get_mega_files()
+
+'''
+TODO:   
+    
+    add movie 
+        ask for url,make sure PIA is running, download torrent, zip file up, upload to mega
+        
+    status of torrent download
+
+    prompt for a movie to delete if no room to add 
+'''
